5f0d824c3981394ffb7ebbbca0b7a9d0b5ac793f
Valentin Gonzalez <101377457+Tintaa307@users.noreply.github.com>
2022-06-22 00:55:15 -0300

    home title finish

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import logo from "./logo.svg"
 import "./App.css"
 import { Nav } from "./components/nav/Nav"
 import Home from "./components/home/Home"
+import { motion } from "framer-motion"
 
 function App() {
   return (
--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,8 +1,140 @@
 import React from "react"
 import "./home.css"
+import { motion } from "framer-motion"
 
 const Home = () => {
-  return <div>Home</div>
+  return (
+    <main className="container-home">
+      <div className="container-content">
+        <div className="container-title">
+          <motion.h1 aria-label="Frontend Developer">
+            <motion.span
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              transition={{ duration: 0.7, delay: 0.1 }}
+            >
+              F
+            </motion.span>
+            <motion.span
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              transition={{ duration: 0.7, delay: 0.2 }}
+            >
+              r
+            </motion.span>
+            <motion.span
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              transition={{ duration: 0.7, delay: 0.3 }}
+            >
+              o
+            </motion.span>
+            <motion.span
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              transition={{ duration: 0.7, delay: 0.4 }}
+            >
+              n
+            </motion.span>
+            <motion.span
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              transition={{ duration: 0.7, delay: 0.5 }}
+            >
+              t
+            </motion.span>
+            <motion.span
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              transition={{ duration: 0.7, delay: 0.6 }}
+            >
+              e
+            </motion.span>
+            <motion.span
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              transition={{ duration: 0.7, delay: 0.7 }}
+            >
+              n
+            </motion.span>
+            <motion.span
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              transition={{ duration: 0.7, delay: 0.8 }}
+            >
+              d
+            </motion.span>
+            <br />
+            <span className="container-2">
+              <motion.span
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                transition={{ duration: 0.7, delay: 0.9 }}
+              >
+                D
+              </motion.span>
+              <motion.span
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                transition={{ duration: 0.7, delay: 1 }}
+              >
+                e
+              </motion.span>
+              <motion.span
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                transition={{ duration: 0.7, delay: 1.1 }}
+              >
+                v
+              </motion.span>
+              <motion.span
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                transition={{ duration: 0.7, delay: 1.2 }}
+              >
+                e
+              </motion.span>
+              <motion.span
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                transition={{ duration: 0.7, delay: 1.3 }}
+              >
+                l
+              </motion.span>
+              <motion.span
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                transition={{ duration: 0.7, delay: 1.4 }}
+              >
+                o
+              </motion.span>
+              <motion.span
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                transition={{ duration: 0.7, delay: 1.5 }}
+              >
+                p
+              </motion.span>
+              <motion.span
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                transition={{ duration: 0.7, delay: 1.6 }}
+              >
+                e
+              </motion.span>
+              <motion.span
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                transition={{ duration: 0.7, delay: 1.7 }}
+              >
+                r
+              </motion.span>
+            </span>
+          </motion.h1>
+        </div>
+      </div>
+    </main>
+  )
 }
 
 export default Home
